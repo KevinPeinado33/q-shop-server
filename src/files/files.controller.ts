c6e37bf179add7c2dd039f7a7dc99fe409aeddc0
KevinPeinado33 <kevinpeinado@upeu.edu.pe>
2023-03-05 12:58:34 -0500

    pruebita

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,17 +1,38 @@
-import { BadRequestException, Controller, Post, UploadedFile, UseInterceptors, Get } from '@nestjs/common'
+import { Response } from 'express'
+import { 
+  BadRequestException, 
+  Controller, 
+  Post, 
+  UploadedFile, 
+  UseInterceptors, 
+  Get, 
+  Param, 
+  Res 
+} from '@nestjs/common'
 import { FileInterceptor } from '@nestjs/platform-express'
 import { diskStorage } from 'multer'
 
 import { FilesService } from './files.service'
 import { fileFilter, fileNamer } from './helpers'
+import { ConfigService } from '@nestjs/config'
 
 @Controller('files')
 export class FilesController {
 
-  constructor(private readonly filesService: FilesService) { }
+  constructor(
+    private readonly filesService: FilesService,
+    private readonly configService: ConfigService
+  ) { }
 
-  @Get(':type')
-  findProductImage() {
+  @Get('product/:imageName')
+  findProductImage(
+    @Res() response              : Response,
+    @Param('imageName') imageName: string
+  ) {
+
+    const path = this.filesService.getStaticProductImage( imageName )
+
+    response.sendFile( path )
 
   }
 
@@ -30,6 +51,9 @@ export class FilesController {
 
     if ( !file )
       throw new BadRequestException('Make sure that the file is an image')
+
+    const hostname = this.configService.get('HOST_API')
+    const secureURL = `${ hostname }:3000/api/file/product/${ file.filename }`
 
     return {
       fileName: file.originalname
